7b2b0436cd0c6f93d0f9dcf37da036e7d79bc2cc
Savvas Melidonis <79579567+SavvasMel@users.noreply.github.com>
2026-02-20 12:38:14 +0100

    Fixing lead time dimension. Linting (#1891)

--- packages/evaluate/src/weathergen/evaluate/utils/utils.py
+++ packages/evaluate/src/weathergen/evaluate/utils/utils.py
@@ -25,7 +25,6 @@ from weathergen.evaluate.plotting.plot_utils import (
     bar_plot_metric_region,
     heat_maps_metric_region,
     plot_metric_region,
-    quantile_plot_metric_region,
     ratio_plot_metric_region,
     score_card_metric_region,
 )
@@ -33,7 +32,6 @@ from weathergen.evaluate.plotting.plotter import (
     BarPlots,
     LinePlots,
     Plotter,
-    QuantilePlots,
     ScoreCards,
 )
 from weathergen.evaluate.scores.score import VerifiedData, get_score
@@ -634,7 +632,7 @@ def plot_summary(cfg: dict, scores_dict: dict, summary_dir: Path):
     plotter = LinePlots(plot_cfg, summary_dir)
     sc_plotter = ScoreCards(plot_cfg, summary_dir)
     br_plotter = BarPlots(plot_cfg, summary_dir)
-    quantile_plotter = QuantilePlots(plot_cfg, summary_dir)
+    # quantile_plotter = QuantilePlots(plot_cfg, summary_dir)
     plotting_log_emitted = False
     for region in regions:
         for metric in metrics:
